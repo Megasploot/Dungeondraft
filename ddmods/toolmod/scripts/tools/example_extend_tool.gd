cece1240092c748d42d1353797226c0d11ca94b0
Megasploot <48600831+Megasploot@users.noreply.github.com>
2023-01-19 14:49:50 -0800

    Add files via upload

---
+++ ddmods/toolmod/scripts/tools/example_extend_tool.gd
@@ -0,0 +1,43 @@
+var script_class = "tool"
+
+# the following variables are not necessary. you can declare more yourself
+var select_tool = null
+
+
+# this method is required. it is called just after the script is loaded.
+func start():
+    var tool_panel = Global.Editor.Toolset.GetToolPanel("SelectTool")
+    var button = tool_panel.CreateButton("Straighten", Global.Root + "icons/question.png")
+    # moving a button from the bottom to the top of the panel
+    tool_panel.Align.move_child(button, 0)
+    button.connect("pressed", self, "straighten")
+    # cache the select tool to make it easier to reference in this script
+    select_tool = Global.Editor.Tools["SelectTool"]
+
+# this method is not necessary. it just shows you how to tell every tick if a tool is enabled
+func update(delta):
+    if select_tool == Global.Editor.ActiveTool:
+        pass
+        # print("!")
+        # uncomment the previous line if you want to see in the DungeondraftConsole whenever select tool is active
+
+func straighten():
+    # Selected is an array of selected objects
+    print(select_tool.Selected)
+    print("TEST3")
+    # Selectables is a dictionary of the selected objects as keys and their types as values
+    var average = Vector2.ZERO
+    var count = 0
+    for thing in select_tool.Selectables:
+        print(select_tool.Selectables[thing])
+        # 4 is object types
+        if select_tool.Selectables[thing] == 4:
+            average += thing.global_position
+            count += 1
+    # prevent divide by zero
+    if count > 0:
+        average /= count
+        # align horizontally at the average height
+        for thing in select_tool.Selectables:
+            if select_tool.Selectables[thing] == 4:
+                thing.global_position.x = average.x
