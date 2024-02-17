47174cb1823e85be7eef51961e3fceff8d89c8eb
Megasploot <48600831+Megasploot@users.noreply.github.com>
2024-02-17 14:23:23 -0800

    Add files via upload

--- ddmods/toolmod/scripts/tools/example_new_tool.gd
+++ ddmods/toolmod/scripts/tools/example_new_tool.gd
@@ -18,6 +18,10 @@ func start():
     button1.connect("pressed", self, "show_dialog_box")
     var button2 = tool_panel.CreateButton("Randomize Asset", Global.Root + "icons/add.png")
     button2.connect("pressed", self, "randomize_asset")
+    var button3 = tool_panel.CreateButton("Instance Object", Global.Root + "icons/add.png")
+    button3.connect("pressed", self, "instance_object")
+    var button4 = tool_panel.CreateButton("Create Undo History", Global.Root + "icons/add.png")
+    button4.connect("pressed", self, "create_custom_history")
 
 # this method is automatically called every frame. delta is a float in seconds. can be removed from script.
 func update(delta : float):
@@ -53,3 +57,15 @@ func randomize_asset():
     Global.Editor.Tools["ObjectTool"].Preview.Texture = Script.GetAssetTexture("Objects", asset_list[r])
     # pulls color from the object tool selection and hands it off to the object instance
     Global.Editor.Tools["ObjectTool"].PromoteCustomColor()
+
+func instance_object():
+    # create an uninteractable object in the middle of the screen to showcase the new instance extension feature
+    var sprite = Script.InstanceExtension("library/mysprite.gd", Global.World.Level.Objects)
+    sprite.texture = load(Global.Root + "icons/example_tool.png")
+    sprite.scale = Vector2(8, 8)
+    sprite.position = Global.World.WoxelDimensions * 0.5
+
+func create_custom_history():
+    var record_script = Script.InstanceReference("library/custom_history_record.gd")
+    record_script.state["test"] = true
+    var record = Global.Editor.History.CreateCustomRecord(record_script)
